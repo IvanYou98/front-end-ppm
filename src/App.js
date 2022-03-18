6ef11e1c563ad85dd7572f65f3c2208c6a60be04
IvanYou98 <yuanfanyou@gmail.com>
2022-03-18 10:44:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
             <BrowserRouter>
                 <div>
                     <NavBar/>
+                    <Route path="/" exact component={props => <Dashboard {...props} />}/>
                     <Route path="/dashBoard" exact component={props => <Dashboard {...props} />}/>
                     <Route path="/addProject" exact component={props => <AddProject {...props} />}/>
                     <Route path="/updateProject/:id" exact component={props => <UpdateProject {...props} />}/>
